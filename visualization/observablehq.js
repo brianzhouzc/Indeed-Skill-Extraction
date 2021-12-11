93f1ed5a54a792ac36c06ab5c726dd77c4e06163
Brian Zhou <brianzhouzc@gmail.com>
2021-12-10 23:42:12 -0800

    Added Chord diagram

--- visualization/observablehq.js
+++ visualization/observablehq.js
@@ -4,6 +4,11 @@ new Runtime().module(define, name => {
     if (name === "chart") return new Inspector(document.querySelector("#observablehq-chart-96e916e8"));
 });
 
+import cdc_define from "https://api.observablehq.com/d/541c1454d475aa9b.js?v=3";
+new Runtime().module(cdc_define, name => {
+    if (name === "chart") return new Inspector(document.querySelector("#observablehq-chart-9614742a"));
+});
+
 import define_wc from "https://api.observablehq.com/d/c5ed13cfdb7003f5.js?v=3";
 new Runtime().module(define_wc, name => {
     if (name === "sdechart")
